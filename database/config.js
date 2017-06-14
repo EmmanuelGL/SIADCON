a2bf43f041394ecd09d5cf1459521b27519602c3
Emmas <emmas1900@hotmail.com>
2017-06-13 22:23:55 -0500

    casi

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 var config = {
+   "url": process.env.DB || "mongodb://User:1234@ds149207.mlab.com:49207/siadcon"
    // "url": process.env.DB || "mongodb://carlos:crispin@ds131511.mlab.com:31511/condominio" 
-    "url": process.env.DB || "mongodb://127.0.0.1:27017/Condominio" 
-    
+   // "url": process.env.DB || "mongodb://127.0.0.1:27017/Condominio"  
 };
 
 module.exports = config
